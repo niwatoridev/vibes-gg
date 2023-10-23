2bdba0a6be8bf9c9101ce9426573fb6cd5e8a406
niwatoridev <esmunozdev@gmail.com>
2023-10-22 19:41:04 -0600

    feat(*): Convert Body sections in components [VIBESGG]

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,5 @@
 import './Home.css';
 import { useEffect, useRef, useState } from 'react';
-import headerVideo from "./media/header-video.mp4"
 import TransparentNavBar from '../../components/NavBar/TransparentNavBar';
 import NavBar from '../../components/NavBar/NavBar';
 
@@ -9,26 +8,14 @@ import downArrow from '../../media/downArrow.png'
 import { HashLink } from 'react-router-hash-link';
 import ExpandingCards from '../../components/ExpandingCards/ExpandingCards';
 import AboutUs from '../../components/AboutUs/AboutUs';
+import VideoHeader from '../../components/VideoHeader/VideoHeader';
 
 
 
 function Home() {
 
-const videoHeader = useRef(null)
 const [navIsTransparent, setNavIsTransparent] = useState(true)
 const videoRef = useRef();
-const homeRef = useRef();
-
-
-const attemptPlay = () => {
-  videoHeader && 
-  videoHeader.current &&
-  videoHeader.current.play().catch(error => console.error("Error attempting to autoplay Header Video", error))
-}
-
-useEffect(() => {
-  attemptPlay();
-}, []);
 
 useEffect(()=>{
 const observer = new IntersectionObserver((entries)=>{
@@ -39,28 +26,22 @@ const observer = new IntersectionObserver((entries)=>{
 observer.observe(videoRef.current)
 }, [])
 
-
-
   return (
     <div className="App">
       <div className='navBar'>
         {navIsTransparent ? <TransparentNavBar/> : <NavBar/>}
       </div>
       <div className='header'>
-        <div ref={homeRef} id='home'></div>
-        <div className='videoContainer'>
-          <div ref={videoRef} className='detectorNav'></div>
-          <video ref={videoHeader} src={headerVideo} loop muted alt="Header Video" id='headerVideo'></video>
-          <HashLink to="#aboutUs" smooth><img src={downArrow} className='downArrow' id='downArrowAboutUs' alt="downArrow" /></HashLink>
-        </div>
+        <div id='home'></div>
+        <div ref={videoRef} className='detectorNav'></div>
+        <VideoHeader></VideoHeader>
+        <HashLink to="#aboutUs" smooth><img src={downArrow} className='downArrow' id='downArrowAboutUs' alt="downArrow" /></HashLink>
       </div>
       <div className='body'>
         <div id='aboutUs'></div>
           <AboutUs></AboutUs>
-        <div className='projectsContainer' id='projects'>
-          <h1>Proyectos</h1>
+        <div id='projects'></div>
           <ExpandingCards></ExpandingCards>
-        </div>
         <div className='contactContainer' id='contactUs'>
 
           <h1>Contacto</h1>
